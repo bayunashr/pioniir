97b56641bc5eb55d29859e8c7bc7d48283a006ee
Bayu Nashrullah <nashr.bayu77@gmail.com>
2023-12-10 13:47:54 +0700

    update admin & user seeder password to 123

--- app/Database/Seeds/AdminSeeder.php
+++ app/Database/Seeds/AdminSeeder.php
@@ -12,7 +12,7 @@ class AdminSeeder extends Seeder
             'adminId'       => '3184a693-37b1-4a40-b7e8-e60fef26e8d3',
             'adminName'     => 'Administrator',
             'adminEmail'    => 'admin@pioniir.com',
-            'adminPassword' => 'pioniir'
+            'adminPassword' => '$2a$12$G3L734KFl92IrEpBuWkMzOH53fpC35axI3V8gWnXnK6JctzqWDDcO'
         ];
 
         $this->db->table('Admin')->insert($data);
