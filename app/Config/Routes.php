0f7ef5a96de11a232370ff0f5157c5e93c856abe
rinoZakharia <rino.zakharia14@gmail.com>
2023-12-17 01:46:53 +0700

    add route content

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,7 @@ $routes->group('/', function ($routes) {
     $routes->get('creator/(:any)', 'Home::profilPage/$1');
     $routes->get('post/(:any)', 'Home::profilPost/$1');
     $routes->get('content/(:any)', 'Home::profilContent/$1');
+    $routes->get('view/content/(:any)', 'Home::contentView/$1');
     $routes->post('donate', 'Midtrans::donate');
 
     //Perlu filter
