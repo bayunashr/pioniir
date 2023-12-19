4301c4ebd6b0be111be54ef0507435e6f1760d5a
rinoZakharia <rino.zakharia14@gmail.com>
2023-12-19 20:36:07 +0700

    add error view

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -3,82 +3,50 @@
 <head>
     <meta charset="utf-8">
     <title><?= lang('Errors.pageNotFound') ?></title>
-
-    <style>
-        div.logo {
-            height: 200px;
-            width: 155px;
-            display: inline-block;
-            opacity: 0.08;
-            position: absolute;
-            top: 2rem;
-            left: 50%;
-            margin-left: -73px;
-        }
-        body {
-            height: 100%;
-            background: #fafafa;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            color: #777;
-            font-weight: 300;
-        }
-        h1 {
-            font-weight: lighter;
-            letter-spacing: normal;
-            font-size: 3rem;
-            margin-top: 0;
-            margin-bottom: 0;
-            color: #222;
-        }
-        .wrap {
-            max-width: 1024px;
-            margin: 5rem auto;
-            padding: 2rem;
-            background: #fff;
-            text-align: center;
-            border: 1px solid #efefef;
-            border-radius: 0.5rem;
-            position: relative;
-        }
-        pre {
-            white-space: normal;
-            margin-top: 1.5rem;
-        }
-        code {
-            background: #fafafa;
-            border: 1px solid #efefef;
-            padding: 0.5rem 1rem;
-            border-radius: 5px;
-            display: block;
-        }
-        p {
-            margin-top: 1.5rem;
-        }
-        .footer {
-            margin-top: 2rem;
-            border-top: 1px solid #efefef;
-            padding: 1em 2em 0 2em;
-            font-size: 85%;
-            color: #999;
-        }
-        a:active,
-        a:link,
-        a:visited {
-            color: #dd4814;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
+    <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+    <meta name="author" content="elemis">
+    <title>Pioniir - Creator Homebase</title>
+    <link rel="shortcut icon" href="<?= base_url() ?>assets/front/img/pioniir.png">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/front/css/plugins.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/front/css/style.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/front/css/colors/aqua.css">
 </head>
 <body>
-    <div class="wrap">
-        <h1>404</h1>
-
-        <p>
-            <?php if (ENVIRONMENT !== 'production') : ?>
-                <?= nl2br(esc($message)) ?>
-            <?php else : ?>
-                <?= lang('Errors.sorryCannotFind') ?>
-            <?php endif; ?>
-        </p>
+    <div class="content-wrapper">
+    <section class="wrapper bg-light">
+        <div class="container pt-10 pt-md-12 pb-12 pb-md-14">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-6 mx-auto px-10 px-sm-0 mb-5">
+                    <figure class=""><img class="img-fluid" src="<?= base_url() ?>assets/front/img/404.webp" alt=""></figure>
+                </div>
+                <!-- /column -->
+                <div class="col-sm-12 col-md-12 col-lg-6 mx-auto my-auto text-center">
+                    <img class="img-fluid w-50 mb-10 mx-auto d-none d-lg-block d-md-none d-sm-none" src="<?= base_url() ?>assets/front/img/pioniirdark.png" alt="">
+                    <h1 class="mb-3">Oops! Page Not Found.</h1>
+                    <p class="lead mb-5 px-md-7 px-lg-5 px-xl-7">The page you are looking for is not available or has been moved. Try a different page or go to homepage with the button below.</p>
+                    <a href="<?= base_url() ?>" class="btn btn-primary rounded-pill">Go to Homepage</a>
+                </div>
+                <!-- /column -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /section -->
     </div>
+
+    <!-- <p> -->
+        <?//php if (ENVIRONMENT !== 'production') : ?>
+            <?// nl2br(esc($message)) ?>
+        <?//php else : ?>
+            <?//= lang('Errors.sorryCannotFind') ?>
+        <?//php endif; ?>
+    <!-- </p> -->
+    
+    <script src="<?= base_url() ?>assets/front/js/plugins.js"></script>
+    <script src="<?= base_url() ?>assets/front/js/theme.js"></script>
 </body>
 </html>
